868f39179db4a08e1f9ea29d79cfc7306e734dde
Sven Wagener <sven@awesome.ug>
2018-03-22 16:32:31 +0100

    Added Admin_Notices trait

---
+++ src/wp/admin_notices.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Skip\WP;
+
+/**
+ * Trait WP_Enqueue_Scripts
+ *
+ * @package Skip\WP
+ */
+trait Admin_Notices {
+
+	/**
+	 * Admin Notices
+	 *
+	 * @var array
+	 *
+	 * @since 1.0.0
+	 */
+	private $admin_notices = array();
+
+	/**
+	 * Initialising admin notices trait, needed started one time for showing
+	 *
+	 * @since 1.0.0
+	 */
+	protected function admin_notices() {
+		add_action( 'admin_notices', array( $this, 'show_notices' ) );
+	}
+
+	/**
+	 * Adding a notice to show
+	 *
+	 * @param string $message Message to show
+	 * @param string $type Notice type notice-error, notice-warning, notice-success or notice-info.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function add_notice( $message, $type = 'notice-info' ) {
+		$this->admin_notices[ $type ][] = $message;
+	}
+
+	/**
+	 * Showing admin notices
+	 *
+	 * @since 1.0.0
+	 */
+	private function show_admin_notices() {
+		foreach( $this->admin_notices AS $type ) {
+			$html = '<div class="notice ' . $type . ' is-dismissible">';
+			foreach( $type AS $message ) {
+				$html.= '<p>' . $message . '</p>';
+			}
+			$html.= '</div>';
+		}
+
+		echo $html;
+	}
+
+	/**
+	 * Hiding methods from IDE
+	 *
+	 * @param string $method Method name
+	 * @param array $arguments Arguments to pass
+	 *
+	 * @since 1.0.0
+	 */
+	public function __call( $method, $arguments ) {
+		switch( $method ) {
+			case 'show_admin_notices':
+				$this->show_admin_notices();
+				break;
+		}
+	}
+}
