2cce0192263d02e5e37d08f2435d2850ba2b3eea
Sven Wagener <sven@awesome.ug>
2018-04-05 16:40:09 +0200

    Added function to add path an to set file prefix

--- src/wp/template_loader.php
+++ src/wp/template_loader.php
@@ -22,6 +22,15 @@ class Template_Loader{
 	private $arguments = array();
 
 	/**
+	 * Template prefix for templates files
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.0
+	 */
+	private $template_prefix;
+
+	/**
 	 * Template name
 	 *
 	 * @var string
@@ -29,6 +38,9 @@ class Template_Loader{
 	 * @since 1.0.0
 	 */
 	private $template_name;
+
+
+	private $template_dirs = array();
 
 	/**
 	 * Template_Loader constructor.
@@ -51,7 +63,31 @@ class Template_Loader{
 			throw new Skip_Exception( 'Empty value for parameter 1 is not allowed' );
 		}
 
+		$this->template_dirs = array(
+			get_template_directory()
+		);
+
+		$this->init();
+
 		return $this->load();
+	}
+
+	/**
+	 * Initializing point for sub classes (maybe to add template path etc.)
+	 *
+	 * @since 1.0.0
+	 */
+	protected function init() {}
+
+	/**
+	 * Adds path to search for templates
+	 *
+	 * @param string $path
+	 *
+	 * @since 1.0.0
+	 */
+	public function add_template_path( $path ) {
+		$this->template_dirs[] = $path;
 	}
 
 	/**
@@ -84,6 +120,17 @@ class Template_Loader{
 		return $this->arguments;
 	}
 
+	/**
+	 * Get specific argument
+	 *
+	 * @param $num
+	 *
+	 * @return mixed
+	 */
+	protected function get_argument( $num ) {
+		return $this->arguments[ $num ];
+	}
+
 
 	/**
 	 * Get template name
@@ -94,5 +141,46 @@ class Template_Loader{
 	 */
 	protected function get_template_name() {
 		return $this->template_name;
+	}
+
+	/**
+	 * Setting template prefix which is used before template filenames
+	 *
+	 * @param string $prefix
+	 *
+	 * @since 1.0.0
+	 */
+	protected function set_template_prefix( $prefix ) {
+		$this->template_prefix = $prefix;
+	}
+
+	/**
+	 * Get template prefix
+	 *
+	 * @return string
+	 *
+	 * @since 1.0.0
+	 */
+	protected function get_template_prefix() {
+		return $this->template_prefix;
+	}
+
+	/**
+	 * Locate template
+	 *
+	 * @return string Filename with full path
+	 *
+	 * @since 1.0.0
+	 */
+	protected function locate_template() {
+		$template_filename = $this->get_template_name() . 'php';
+
+		foreach( $this->template_dirs AS $dir ) {
+			$filename = $dir . '/' . $template_filename;
+
+			if( file_exists( $filename ) ) {
+				return $filename;
+			}
+		}
 	}
 }
